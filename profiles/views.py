626470ac65329f22d10e667a64858bfe1b26450b
Paulo Arbeláez <pauloarbelaez@gmail.com>
2023-12-02 07:47:08 +0000

    Create the put method for the profile detail

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -16,6 +17,9 @@ class ProfileView(APIView):
         return Response(serializer.data)
 
 class ProfileDetail(APIView):
+    # We need to define the serializer class here to be able to have a proper
+    # form in the browsable API
+    serializer_class = ProfileSerializer
     # We use get_object to retrieve a single profile
     def get_object(self, pk):
         return get_object_or_404(Profile, pk=pk)
@@ -24,3 +28,13 @@ class ProfileDetail(APIView):
         profile = self.get_object(pk)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+
+    # We use put to update a single profile
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        # if the serializer is not valid, return a 400 error
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
