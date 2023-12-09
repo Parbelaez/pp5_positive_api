5a1c5cde6e55b67ac2805d36618f05c098a18bff
Paulo Arbeláez <pauloarbelaez@gmail.com>
2023-12-09 10:58:00 +0000

    Add the localhost (127.0.0.1) to the allowed hosts

--- positive_api/settings.py
+++ positive_api/settings.py
@@ -64,7 +64,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ and os.environ['DEV'] == 'True'
 
-ALLOWED_HOSTS = ['localhost', os.environ.get('ALLOWED_HOST'),]
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    os.environ.get('ALLOWED_HOST'),]
 
 
 # Application definition
