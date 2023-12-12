c441c3485e09351abac17526e44de09288d3765e
Paulo Arbeláez <pauloarbelaez@gmail.com>
2023-12-12 11:37:37 +0000

    Fix the usage of the serializer in the class

--- positive_api/serializers.py
+++ positive_api/serializers.py
@@ -2,11 +2,11 @@ from dj_rest_auth.serializers import UserDetailsSerializer
 from rest_framework import serializers
 
 
-class CurrentUserDetailSerializer(UserDetailSerializer):
-    profile_id = serializers.IntegerField(source='profile.id')
-    profile_image = serializers.ImageField(source='profile.image.url')
+class CurrentUserSerializer(UserDetailsSerializer):
+    profile_id = serializers.ReadOnlyField(source='profile.id')
+    profile_image = serializers.ReadOnlyField(source='profile.image.url')
 
-    class Meta(UserDetailSerializer.Meta):
-        fields = UserDetailSerializer.Meta.fields + (
+    class Meta(UserDetailsSerializer.Meta):
+        fields = UserDetailsSerializer.Meta.fields + (
             'profile_id', 'profile_image'
         )
