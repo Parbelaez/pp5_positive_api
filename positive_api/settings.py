5c0d622f422c72de2c77f65dbc6eb808ff2eb186
Paulo Arbeláez <pauloarbelaez@gmail.com>
2023-12-13 09:52:46 +0000

    Add whitenoise for admin panel in Heroku

--- positive_api/settings.py
+++ positive_api/settings.py
@@ -118,6 +118,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     # 'allauth.account.middleware.AccountMiddleware',
 ]
 
@@ -211,6 +212,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
