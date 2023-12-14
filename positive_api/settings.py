d26216041ca3e72da4570b0a9a1215f65d27eb49
Paulo Arbeláez <pauloarbelaez@gmail.com>
2023-12-14 23:52:32 +0000

    Comment out CSRF

--- positive_api/settings.py
+++ positive_api/settings.py
@@ -70,11 +70,11 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     ]
 
-CSRF_TRUSTED_ORIGINS=[
-    'http://127.0.0.1:8000',
-    'https://*.herokuapp.com',
-    'https://*.gitpod.io',
-]
+# CSRF_TRUSTED_ORIGINS=[
+#     'http://127.0.0.1:8000',
+#     'https://*.herokuapp.com',
+#     'https://*.gitpod.io',
+# ]
 
 # Application definition
 
