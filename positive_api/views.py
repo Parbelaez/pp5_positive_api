23f29b2ec0c768396782357b5bfdc405c6322aa3
Paulo Arbeláez <pauloarbelaez@gmail.com>
2023-12-16 21:31:41 +0000

    created custom user view without permissions

--- positive_api/views.py
+++ positive_api/views.py
@@ -1,10 +1,17 @@
+import logging
+from django.contrib.auth import get_user_model
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import permissions
+from dj_rest_auth.views import UserDetailsView
 # dj-rest-auth bug fix workaround
 from .settings import (
     JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
     JWT_AUTH_SECURE,
 )
+
+logger = logging.getLogger(__name__)
 
 
 @api_view()
@@ -36,3 +43,18 @@ def logout_route(request):
         secure=JWT_AUTH_SECURE,
     )
     return response
+
+
+class CustomUserDetailsView(UserDetailsView):
+    permission_classes = (
+        permissions.AllowAny,
+    )
+    def get_object(self):
+        return self.request.user
+
+    def get_queryset(self):
+        """
+        Adding this method since it is sometimes called when using
+        django-rest-swagger
+        """
+        return get_user_model().objects.none()
